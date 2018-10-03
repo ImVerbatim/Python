dace22449a7a869334b2f527253cf51638d5e050
A.J. Delcimmuto <delcimaj@mail.uc.edu>
2018-10-03 14:09:39 -0400

    finished with hw3

--- HW3/hw03.py
+++ HW3/hw03.py
@@ -1,29 +1,42 @@
+__author__ = "Bart Simpson" # A.J. DelCimmuto
+__credits__ = ["Lisa Simpson", "Santa's Little Helper"] # No One
+__email__ = "bs123@mail.uc.edu" # delcimaj@mail.uc.edu
+
+
+
 words = open('words.txt',encoding='ascii').read().upper().split()
 
 
 def step(word):
-    for aWord in words:    #iterate through the list of words 
-        for letter in word:    #iterate through the letters of the 
-            if checkLetters(aWord,letter) != -1:
-                aWord.replace(i,"")
-                continue
+    answer = []
+    for aWord in words:   #iterate through the list of words 
+
+        saveWord = aWord  #save the word because the program edits the word to eliminate repeated characters
+
+        letterCount = 0   #count the ammount of times a letter appears in a word
+
+        for letter in word: #iterate through the letters of the input word
+
+            if letter in aWord:    #check if letter is in a word in the list
+
+                letterCount += 1      #count how many times the letters match        
+                    
+                aWord = aWord.replace(letter,'',1) #remove each letter that matches. This allows for multiple letter inputs such as "APPLE" (two P's)
+
             else:
+           
                 break
-    return aWord
+                
+
+        if letterCount >= len(word) and len(saveWord) == len(word) + 1:
             
+            answer.append(saveWord)  #add to list
+                
+    return answer
+        
 
 
-def checkLetters(aWord,letter):
-    i = 0 
-    while i < len(aWord):
-        if letter == aWord[i]:
-            return i
-        return -1
-
-
-
-
-print(step("apple"))
+print(step("APPLE"))
 
 
 
